f2da3633e27ff1b3d48240c3fb6047f7f3865339
Shawn Kilcoyne <sbsk94@gmail.com>
2015-04-19 15:16:40 -0400

    upload

---
+++ src/router.js
@@ -0,0 +1,17 @@
+//import the controller folder (automatically calls the index.js file)
+var controllers = require('./controllers'); 
+var mid = require('./middleware');
+
+var router = function(app) {
+
+    app.get("/login", mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage); 
+    app.post("/login", mid.requiresSecure, mid.requiresLogout, controllers.Account.login); 
+    app.get("/signup", mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
+    app.post("/signup", mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
+    app.get("/logout", mid.requiresLogin, controllers.Account.logout);
+    app.get("/maker", mid.requiresLogin, controllers.Domo.makerPage);
+    app.post("/maker", mid.requiresLogin,  controllers.Domo.make);
+    app.get("/", mid.requiresSecure, controllers.Account.loginPage);
+};
+
+module.exports = router; 
